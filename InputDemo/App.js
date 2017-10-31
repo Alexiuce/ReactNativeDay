48d4c396cfd6cd9b3c5136d7f9a8cf2088f58989
Alexiuce <alexiuce@qq.com>
2017-10-31 13:41:59 +0800

    setup ui input

--- InputDemo/App.js
+++ InputDemo/App.js
@@ -5,73 +5,17 @@
  */
 
 import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View,
-  TextInput,
 
-} from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+// var LoginView = require('./LoginView');
+
+import LoginView from "./LoginView"
 
 export default class App extends Component<{}> {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-          <TextInput style={styles.inputStyle}
-                     // 设置默认文字
-                     // value={"I am dev"}
-                     // 设置键盘类型
-                     //keyboardType={"number-pad"}
-
-                     // 设置多行
-                     multiline={true}
-                     // 占位文字
-                     placeholder={"please input in here"}
-          />
-
-      </View>
+        <LoginView/>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-    inputStyle:{
-      width:300,
-        height:30,
-        borderWidth:1,
-        borderColor:'green'
-    },
-});
---
+++ InputDemo/LoginView.js
@@ -0,0 +1,92 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Image,
+} from 'react-native';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' +
+  'Cmd+D or shake for dev menu',
+  android: 'Double tap R on your keyboard to reload,\n' +
+  'Shake or press menu button for dev menu',
+});
+
+export default class LoginView extends Component<{}> {
+  render() {
+    return (
+        <View style={styles.container}>
+          {/* 头像*/}
+          <Image source={require('./img/icon.png')} style={styles.iconStyle}/>
+          {/* 用户名和密码*/}
+          <TextInput style={styles.inputStyle}
+                     placeholder="请输入账号"
+          />
+          <TextInput style={styles.inputStyle}
+                     placeholder="密码"
+          />
+          {/* 设置*/}
+          <View style={styles.settingViewStyle}>
+            <Text>忘记密码</Text>
+            <Text>注册用户</Text>
+          </View>
+          {/* 其他登陆*/}
+          <View style={styles.otherViewStyle}>
+            <Text>其他登陆方式123:</Text>
+            <Image source={require('./img/icon3.png')} style={styles.otherImageStyle}/>
+            <Image source={require('./img/icon7.png')}style={styles.otherImageStyle}/>
+            <Image source={require('./img/icon8.png')}style={styles.otherImageStyle}/>
+          </View>
+        </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F5FCFF',
+  },
+  iconStyle:{
+    marginTop:100,
+    width:60,
+    height:60,
+    alignSelf:'center',
+    borderRadius:30,
+  },
+  inputStyle:{
+    width:300,
+    height:30,
+    borderWidth:1,
+    borderColor:'green',
+    alignSelf:'center',
+    marginTop:5,
+  },
+  settingViewStyle:{
+    flexDirection:"row",
+    justifyContent:"space-between",
+    marginTop:15,
+    marginLeft:15,
+    marginRight:15,
+  },
+  otherViewStyle:{
+    flexDirection:'row',
+    alignItems:'center'
+
+  },
+  otherImageStyle:{
+    width: 40,
+    height:40,
+    marginLeft:15,
+  },
+});
+
