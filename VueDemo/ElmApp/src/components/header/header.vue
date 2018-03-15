bc373eedd13f9d9928b383660c9bb9fca653cca4
Alexiuce <alexiuce@qq.com>
2018-03-15 17:38:10 +0800

    updagte header view

--- VueDemo/ElmApp/src/components/header/header.vue
+++ VueDemo/ElmApp/src/components/header/header.vue
@@ -2,33 +2,37 @@
 
  <div class="header-view">
     <!--内容部分-->
-    <div class="content-wrapper">
-      <!--  左边图标-->
-      <div class="avatar-seller">
-        <img :src="seller.avatar"/>
+      <div class="content-wrapper">
+        <!--  左边图标-->
+          <div class="avatar-seller">
+            <img :src="seller.avatar"/>
+          </div>
+        <!-- 右边内容 : 品牌, title, name ,优惠 -->
+          <div class="right-content">
+            <div class="title">
+              <span class="band-icon"></span>
+              <span class="seller-name">{{seller.name}}</span>
+            </div>
+            <!--快递信息-->
+            <div class="delivery-info">
+              {{seller.description + "  /  " + seller.deliveryTime + "分钟" }}
+            </div>
+            <!--优惠支持-->
+            <div class="support" v-if="seller.supports">
+              <span class="support-icon" :class="this.classMap[seller.supports[0].type]"></span>
+              <span class="support-info">{{seller.supports[0].description}}</span>
+            </div>
+          </div>
+        <div v-if="seller.supports" class="support-count">
+          <span>{{seller.supports.length}}个 > </span>
+        </div>
       </div>
-      <!-- 右边内容 : 品牌, title, name ,优惠 -->
-      <div class="right-content">
-        <div class="title">
-          <span class="band-icon"></span>
-          <span class="seller-name">{{seller.name}}</span>
-        </div>
-        <!--快递信息-->
-        <div class="delivery-info">
-          {{seller.description + "  /  " + seller.deliveryTime + "分钟" }}
-        </div>
-        <!--优惠支持-->
-        <div class="support" v-if="seller.supports">
-          <span class="support-icon"></span>
-          <span class="support-info">{{seller.supports[0].description}}</span>
-        </div>
+   <!--底部footer部分-->
+      <div class="bulletin-wrapper">
+        <span class="bulletin-icon"></span>
+        <span class="bulletin-text">{{seller.bulletin}}</span>
+        <span class="bulletin-end"> > </span>
       </div>
-    </div>
-    <!--底部footer部分-->
-    <div class="bulletin-wrapper">
-       {{message}}
-    </div>
-
  </div>
 
 </template>
@@ -46,13 +50,19 @@ export default {
     return {
       message:"hello header"
     }
+  },
+  created() {
+    this.classMap = ["decrease","discount","guarantee","invoice","special"]
   }
 }
 
 </script>
 
 <!-------------------------- 样式部分  ----------------------------->
-<style>
+<style lang="stylus" >
+
+  @import "../../common/stylus/mixin.styl";
+
 
   .header-view{
     display: flex;
@@ -60,7 +70,8 @@ export default {
     font-size: 0px;
     flex-direction: column;
     align-items: flex-start;
-    background-color: #000;
+    background-color: #999;
+    position :relative;
   }
 
   .content-wrapper{
@@ -69,10 +80,14 @@ export default {
 
   .avatar-seller{
     display: inline-block;
+    vertical-align : top;
   }
+
   .avatar-seller img {
     width: 64px;
     height: 64px;
+    border-radius:2px;
+
   }
 
   .right-content{
@@ -83,16 +98,87 @@ export default {
   }
   .title{
     margin: 2px 0px 8px 0px;
+
   }
   .band-icon{
     display: inline-block;
+    vertical-align:top;
     width: 30px;
     height: 18px;
+    bg-image('brand')
+    background-size 30px 18px;
+    background-repeat :no-repeat;
   }
+.seller-name{
+  margin-left :6px;
+  font-size 16px;
+  font-weight:bold;
+}
+.delivery-info{
+  margin-bottom :10px;
+  line-height :12px;
+  font-size :12px;
+}
+
+.support-icon{
+  display :inline-block;
+ vertical-align :top ;
+  width :12px;
+  height :12px;
+  margin-right :4px;
+  background-size 12px 12px;
+}
+.decrease{
+  bg-image('decrease_1')
+}
+.discount{
+  bg-image('discount_1')
+}
+
+.guarantee{
+  bg-image('guarantee_1')
+}
+.invoice{
+  bg-image('invoice_1')
+}
+
+.special{
+  bg-image('special_1')
+}
+
+.support-info{
+  font-size :12px;
+  line-height :10px;
+}
+
+.support-count{
+  position :absolute;
+  right: 12px;
+  bottom :18px;
+  padding :0 8px;
+  line-height 24px;
+  height :24px;
+  border-radius :14px;
+  background-color :rgba(0,0,0,0.2)
+  text-align : center;
+  font-size :10px;
+
+}
 
 
-  .bulletin-wrapper{
-    font-size: 14px;
+.bulletin-wrapper{
+  height :28px;
+  line-height :28px;
+  font-size: 14px;
+  padding :0 100px 0 12px;
+
+  white-space nowrap
+  overflow hidden;
+  text-overflow ellipsis
   }
+
+.bulletin-text{
+
+}
 
 </style>
